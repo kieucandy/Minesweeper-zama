71d64f21b239ecca93e94b38ba04b517f81b8ee3
Kieu <tuanphamit.dev@gmail.com>
2025-10-31 23:01:02 +0700

    Minesweeper zama

--- frontend/src/config/wallet.ts
+++ frontend/src/config/wallet.ts
@@ -7,7 +7,8 @@ const projectId = 'd43bccd656d6c1a2f7c7693caab78e2c'
 const metadata = {
   name: 'Minesweeper',
   description: 'Minesweeper',
-  url: 'https://minesweeper-zama.vercel.app/'
+  url: 'https://minesweeper-zama.vercel.app/',
+  icons: ['https://minesweeper-zama.vercel.app/logo.png']
 }
 
 createAppKit({
